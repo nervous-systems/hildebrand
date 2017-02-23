a42ade8daf44756c56a0f0a6d9d517e4f04526f7
Andrea Richiardi <a.richiardi.work@gmail.com>
2017-02-22 17:03:31 -0800

    Refactor and remove some dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject io.nervous/hildebrand "0.4.5"
+(defproject io.nervous/hildebrand "0.4.6-SNAPSHOT"
   :description "High-level, asynchronous AWS client library"
   :url "https://github.com/nervous-systems/hildebrand"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
   :scm {:name "git" :url "https://github.com/nervous-systems/hildebrand"}
   :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.nervous/eulalie  "0.6.10"]]
+                 [io.nervous/eulalie  "0.6.10" :exclusions [org.clojure/tools.reader org.clojure/core.async org.clojure/clojurescript prismatic/plumbing]]
+                 [andare "0.4.0" :exclusions [org.clojure/clojure org.clojure/tools.reader]]]
   :npm {:dependencies [[bignumber.js "2.4.0"]]}
-  :plugins [[lein-cljsbuild "1.1.4"]
-            [lein-npm       "0.6.2"]]
+  :plugins [[lein-cljsbuild "1.1.4" :exclusions [org.clojure/clojure]]
+            [lein-npm       "0.6.2" :exclusions [org.clojure/clojure]]]
   :cljsbuild
   {:builds [{:id "main"
              :source-paths ["src"]
